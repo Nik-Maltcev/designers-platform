c36623461deef7d5d827aa5197891f77264ffd21
Nik-Maltcev <35464161+Nik-Maltcev@users.noreply.github.com>
2026-04-11 11:49:24 +0300

    add Yandex Metrika counter

--- app/layout.js
+++ app/layout.js
@@ -23,6 +23,14 @@ export default async function RootLayout({ children }) {
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
           rel="stylesheet"
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window,document,'script','https://mc.yandex.ru/metrika/tag.js?id=108495211','ym');ym(108495211,'init',{ssr:true,webvisor:true,clickmap:true,ecommerce:"dataLayer",referrer:document.referrer,url:location.href,accurateTrackBounce:true,trackLinks:true});`,
+          }}
+        />
+        <noscript>
+          <div><img src="https://mc.yandex.ru/watch/108495211" style={{ position: "absolute", left: "-9999px" }} alt="" /></div>
+        </noscript>
       </head>
       <body className="bg-surface text-on-surface font-body antialiased selection:bg-primary-fixed selection:text-on-primary-fixed">
         {/* TopNavBar */}
